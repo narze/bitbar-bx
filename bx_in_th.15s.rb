9e8b49a57f227a66e02545473d421de37009a4cd
Manassarn Manoonchai <manassarn@gmail.com>
2017-09-02 19:26:06 +0700

    :sparkles: More details

--- bx_in_th.15s.rb
+++ bx_in_th.15s.rb
@@ -12,11 +12,36 @@ def run
   url = 'https://bx.in.th/api/'
   response = Net::HTTP.get(URI(url))
   data = JSON.parse(response)[PAIRING_ID.to_s]
-  last_price = data["last_price"]
-  change = data["change"]
-  primary_currency = data["primary_currency"]
-  secondary_currency = data["secondary_currency"]
-  puts "#{primary_currency}-#{secondary_currency} @ #{last_price} (#{change}%)"
+  output(data)
+end
+
+def output(d)
+  primary = d['primary_currency']
+  secondary = d['secondary_currency']
+  last_price = d['last_price']
+  change = d['change']
+  order_book = d['orderbook']
+  bids = order_book['bids']
+  asks = order_book['asks']
+
+  summary = "#{primary}-#{secondary} @ #{last_price} (#{change}%)"
+  details = [
+    "---",
+    "24h volume : #{d['volume_24hours']} #{secondary}",
+    "---",
+    "Buy orders (Bids) @ #{bids['highbid']} #{secondary}",
+    "Volume : #{bids['volume']} #{secondary}",
+    "Total : #{bids['total']} orders",
+    "---",
+    "Sell orders (Asks) @ #{asks['highbid']} #{secondary}",
+    "Volume : #{asks['volume']} #{secondary}",
+    "Total : #{asks['total']} orders",
+  ]
+
+  [
+    summary,
+    *details,
+  ].each(&method(:puts))
 end
 
 begin
